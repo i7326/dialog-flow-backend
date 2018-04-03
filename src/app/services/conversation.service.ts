167c165dae2839dc93ad2ddcc0ae933b4da26c99
Muhammed Irfan Kamaludeen <i7326@live.com>
2018-04-04 02:30:17 +0530

    temp

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -2,12 +2,21 @@ import { Service } from 'typedi';
 import * as apiai from 'apiai';
 import * as request from 'request';
 import * as cheerio from 'cheerio';
+import * as Firestore from '@google-cloud/firestore';
+
 
 @Service()
 export class ConversationService {
   private client: any;
+  private firestore: any;
   constructor() {
     this.client = apiai('47d91671250d44c2bfca074c80403d39');
+    this.firestore = new Firestore({
+      projectId: 'trans-nueron',
+      keyFilename: './src/trans-nueron-e84bc6da9a20.json',
+    });
+
+    this.migrateData();
   }
 
   public textRequest(text: string, session: any): Promise {
@@ -97,5 +106,56 @@ export class ConversationService {
       reject(error);
     });
   });
+    }
+
+
+    private migrateData(){
+      const url = 'https://www.upes.ac.in/college-wise';
+      const collectionRef = this.firestore.collection('courses');
+      collectionRef.get().then( snapshot => {
+        snapshot.forEach((doc) => {
+          doc.ref.delete();
+        })
+      });
+
+      // console.log(documentRef);
+      //
+      return new Promise((resolve, reject) => {
+        request(url, (error, response, html) => {
+          if (!error) {
+            const $ = cheerio.load(html);
+            let school = $('#ctl00_ContentPlaceHolder1_Accordion1 span').map((i, el) => {
+              return $(el).text($(el).text().replace('School of', '').slice(0, -6));
+            });
+            school.each((i, el) => {
+              let courseElement = $(el).parent().parent().next().find('.table-listing');
+
+              courseElement.each((i, el1) => {
+                let courses =[];
+                $(el1).find('a').each((i, elm) => {
+                  let obj = {
+                    name: $(elm).text().trim(),
+                    programme_level: $(el1).find('h2').text().trim(),
+                    school: $(el).text(),
+                    url: `https://www.upes.ac.in/${$(elm).attr('href')}`
+                  }
+                  if($(el1).find('h2').text().match('Part Time')){
+                      obj['programme_type'] = 'Part Time';
+                      obj.programme_level = obj.programme_level.replace(' - Part Time','');
+                  }
+                  collectionRef.add(obj);
+                });
+
+                console.log($(el1).find('h2').text().trim());
+                // course.push({"level": $(el1).find('h2').text().trim(), "courses": courses})
+              });
+
+              resolve(true);
+            });
+
+          }
+          reject(error);
+        });
+      });
     }
 }
